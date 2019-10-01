07b69b8a00d649df6c4e75d19d99f9a759164600
Omar <o.mohammed@morniksa.com>
2019-10-01 19:35:16 +0200

    remove value option

--- src/components/vehicles/filters/VehicleFilter.js
+++ src/components/vehicles/filters/VehicleFilter.js
@@ -6,7 +6,7 @@ function VehicleFilter(props) {
       <label className="label label-default">by status:</label>
 
       <select className="form-control" onChange={props.callback}>
-        <option value=''>All</option>
+        <option></option>
         <option value="connected">Connected</option>
         <option value="disconnected">Disconnected</option>
       </select>
